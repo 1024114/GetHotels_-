0c5d5a459d13a5c4b410c387d94255c56963745e
gongdingding <gongdingding@live.com>
2017-11-06 10:11:16 +0800

    框架，航空故事版80%

---
+++ GetHotels的副本/GetHotels/Controller/Air/AirTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  AirTableViewCell.m
+//  GetHotels
+//
+//  Created by admin1 on 2017/11/2.
+//  Copyright © 2017年 Education. All rights reserved.
+//
+
+#import "AirTableViewCell.h"
+
+@implementation AirTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
