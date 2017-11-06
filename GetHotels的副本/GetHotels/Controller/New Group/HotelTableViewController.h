1fbba9acfa40fabdc6b727d8fd2842091e59a201
admin <877203456@qq.com>
2017-11-06 14:25:25 +0800

    新故事版

---
+++ GetHotels的副本/GetHotels/Controller/New Group/HotelTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  HotelTableViewController.h
+//  GetHotels
+//
+//  Created by admin on 2017/11/6.
+//  Copyright © 2017年 Education. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HotelTableViewController : UITableViewController
+
+@end
---
+++ GetHotels的副本/GetHotels/Controller/New Group/PostedViewController.h
@@ -0,0 +1,13 @@
+//
+//  PostedViewController.h
+//  GetHotels
+//
+//  Created by admin on 2017/11/6.
+//  Copyright © 2017年 Education. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostedViewController : UIViewController
+
+@end
