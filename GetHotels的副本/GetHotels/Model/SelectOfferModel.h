3cde2efe4e5b84825b8ad5219d279e029de01079
gongdingding <gongdingding@live.com>
2017-11-13 16:34:36 +0800

    判断确定能否被点

--- GetHotels的副本/GetHotels/Model/SelectOfferModel.h
+++ GetHotels的副本/GetHotels/Model/SelectOfferModel.h
@@ -10,15 +10,15 @@
 
 @interface SelectOfferModel : NSObject
 
-@property(nonatomic)NSInteger price;
-@property(nonatomic)NSInteger weight;
-@property(strong,nonatomic)NSString *aviationCompany;
-@property(strong,nonatomic)NSString *aviationCabin;
-@property(strong,nonatomic)NSString *inTimeStr;
-@property(strong,nonatomic)NSString *outTimeStr;
-@property(strong,nonatomic)NSString *departure;
-@property(strong,nonatomic)NSString *destination;
-@property(strong,nonatomic)NSString *flightNo;
+@property(nonatomic)NSInteger price;//价格
+@property(nonatomic)NSInteger weight;//重量
+@property(strong,nonatomic)NSString *aviationCompany;//航空公司
+@property(strong,nonatomic)NSString *aviationCabin;//舱位
+@property(strong,nonatomic)NSString *inTimeStr;//出发时间
+@property(strong,nonatomic)NSString *outTimeStr;//到达时间
+@property(strong,nonatomic)NSString *departure;//出发地
+@property(strong,nonatomic)NSString *destination;//目的地
+@property(strong,nonatomic)NSString *flightNo;//航班
 @property(nonatomic)NSInteger ID;
 
 -(instancetype)initWithDict:(NSDictionary *)dict;
